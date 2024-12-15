6c509a04fc411c5c444ddaf998c583e80f9c507d
2cloudyskies <2cloudyskies@gmail.com>
2024-12-15 14:55:01 +0800

    updated backend

--- terraform.tf
+++ terraform.tf
@@ -5,6 +5,12 @@ terraform {
       version = ">=4.14.0"
     }
   }
+  backend "sa" {
+    resource_group_name  = "az400rg"
+    storage_account_name = "az400tfstate12"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
